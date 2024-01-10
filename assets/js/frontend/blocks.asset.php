d7accd10ecf9e01dff23d0971764678c6df7a2ac
Aman <aman@netzoptimize.com>
2024-01-11 03:32:38 +0530

    added build

--- assets/js/frontend/blocks.asset.php
+++ assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'c6074fe80ea8deba5af6');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'e93f2fdbe0093f2620a6');
